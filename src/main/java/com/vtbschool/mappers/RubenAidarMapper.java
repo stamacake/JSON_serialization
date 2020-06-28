d17d9c09e758be689dfaebdf771685a79260af30
MTigra <tikol2008@gmail.com>
2020-06-28 17:24:23 +0300

    Created RubenAidar xsd to our mapper

--- src/main/java/com/vtbschool/mappers/RubenAidarMapper.java
+++ src/main/java/com/vtbschool/mappers/RubenAidarMapper.java
@@ -3,6 +3,7 @@ package com.vtbschool.mappers;
 import com.vtbschool.fromxsd.rubenaidar.*;
 import com.vtbschool.model.Group;
 import com.vtbschool.model.Intern;
+import com.vtbschool.model.TaskTag;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +25,9 @@ public class RubenAidarMapper implements Mapper {
             Intern intern = new Intern();
             intern.setId(member.getId());
             populateNames(intern, member.getName());
+            populateTasks(intern, member.getTasks().getValue());
+
+
             interns.add(intern);
 
         }
@@ -45,5 +49,31 @@ public class RubenAidarMapper implements Mapper {
         intern.setLastName("");
     }
 
+    private void populateTasks(Intern intern, Tasks tasksWrapper) {
+        List<Task> tasks = tasksWrapper.getTask();
+        List<com.vtbschool.model.Task> ourTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            com.vtbschool.model.Task ourTask = new com.vtbschool.model.Task();
+            ourTask.setId(task.getId());
+            ourTask.setName(task.getName());
+
+            List<Tag> tags = task.getTags().getValue().getTag();
+            populateTags(ourTask, tags);
+
+        }
+    }
+
+    private void populateTags(com.vtbschool.model.Task task, List<Tag> tags) {
+        List<TaskTag> taskTags = new ArrayList<>();
+
+        for (Tag tag : tags) {
+            TaskTag taskTag = new TaskTag();
+
+            taskTag.setId(tag.getId());
+            taskTag.setName(tag.getName());
+            taskTags.add(taskTag)
+        }
+        task.setTags(taskTags);
+    }
 
 }
